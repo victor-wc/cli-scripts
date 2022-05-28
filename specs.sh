d24b1088b0030e431aefe4df4db29f7dcd19a947
victor-wc <victorwohlersc@gmail.com>
2022-05-28 09:42:56 -0300

    added cpu and hd functions

--- specs.sh
+++ specs.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 main(){
-    printf "What do you want to know about you pc? (cpu, dram, hd, devices)
-
+    printf "What do you want to know about you pc? (cpu, dram, hd, devices) \n"
+    read -r option
     
     if [ "$option" = "cpu" ]; then
         if [ -z "$option" ]; then
@@ -44,17 +44,56 @@ main(){
 }
 
 cpu(){
-
+    echo "\n"
+    echo "==============================================================="
+    lscpu | awk '{l[NR] = $0} END {
+        print l[1]
+        print "\n"
+        print l[2]
+        print "\n"
+        print l[5]
+        print "\n"
+        print l[14]
+        print "\n"
+        print l[16]
+        }'
+    echo "==============================================================="
 }
 
 dram(){
+    sudo dmidecode -t memory | grep -i size
+    echo "\n"
+    echo "==============================================================="
+    sudo dmidecode -t memory | grep -i size | awk '{l[NR] = $0} END {
+        for (i=1; i<=NR; i+5){
+                print l[i]
+                print "\n"
+            }
+        }'
+    echo "==============================================================="
 
 }
 
 hd(){
-
+    echo "\n"
+    echo "==============================================================="
+    sudo lshw -short -C disk | awk '{l[NR] = $0} END {
+        print l[1]
+        print "\n"
+        for (i=3; i<=NR; i++){
+                print l[i]
+                print "\n"
+            }
+        }'
+    echo "==============================================================="
 }
 
 devices(){
-    
+    lsusb
 }
+
+general_overview(){
+    lshw
+}
+
+main "$@"
